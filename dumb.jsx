70ddb60dfe54189c6ede0625ee0bdc3a84d21d89
JanjacVD <95536506+JanjacVD@users.noreply.github.com>
2023-04-20 14:39:44 +0200

    Update dumb.jsx

--- dumb.jsx
+++ dumb.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 
-export class Modal extends React.Component<null, State>{
-
-    constructor(props:null){
-        super(props);
-        this.state = {
-        currentElement:null
-        }
-        window.modalComponent = this
-
-    }
-    public assignElement(element:JSX.Element){
-        this.setState({
-            currentElement:element
-        })
-    }
-    public removeElement(){
-        this.setState({
-            currentElement:null
-        })
-    }
-    render(): React.ReactNode {
-        return(
-            <div className={this.state.currentElement ? 'modal-full' : 'modal-hidden'}>
-                {this.state.currentElement}
-            </div>
-        )
-    }
+export class Modal extends React.Component<unknown, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentElement: null,
+    };
+    window.modalComponent = this;
+  }
+  public assignElement(element: JSX.Element) {
+    this.setState({
+      currentElement: element,
+    });
+  }
+  public removeElement() {
+    this.setState({
+      currentElement: null,
+    });
+  }
+  render(): React.ReactNode {
+    return (
+      <div
+        className={this.state.currentElement ? "modal-full" : "modal-hidden"}
+      >
+        {this.state.currentElement}
+      </div>
+    );
+  }
 }
 type State = {
-    currentElement: JSX.Element | null;
+  currentElement: JSX.Element | null;
+};
+export function displayElementInModal(element: JSX.Element) {
+  window.modalComponent.assignElement(element);
 }
-export function displayElementInModal(element:JSX.Element){
-    window.modalComponent.assignElement(element)
-}
-export function removeElementFromModal(){
-    window.modalComponent.removeElement()
+export function removeElementFromModal() {
+  window.modalComponent.removeElement();
 }
 declare global {
-    interface Window {
-        modalComponent:Modal;
-    }
+  interface Window {
+    modalComponent: Modal;
+  }
 }
